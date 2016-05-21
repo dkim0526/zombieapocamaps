bddc9eb4576e55acb107d7749b8854126e098e02
Steven Lim <stevenlim890@gmail.com>
2016-05-21 16:46:30 -0700

    find resources almost done

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -13,23 +13,31 @@ $(document).ready(function() {
 	$(".skill_input").change(updateSkillPercent);
 })
 
+var markers = [];
+function createMarker(latlng, map, self){
+	var marker = new google.maps.Marker({
+        position: latlng,
+        map: map
+    });
+    if(!self){
+    	google.maps.event.addListener(marker, 'click', function(){getRouteFromClick(map, marker);});
+    	markers.push(marker);
+    } 
+}
+
+function geoCodeMarkers(addresses, map){
+	for(var x = 0; x < addresses.length; x++) {
+        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+            var p = data.results[0].geometry.location
+            var latlng = new google.maps.LatLng(p.lat, p.lng);
+	        createMarker(latlng, map, false);
+        });
+	}
+}
+
 function openSafetyZones() {
 	openTab($("#safety_zone"), $("#safety_zone_btn"));
 }
-
-/*function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-directionsService.route({
-  origin: document.getElementById('start').value,
-  destination: document.getElementById('end').value,
-  travelMode: google.maps.TravelMode.DRIVING
-}, function(response, status) {
-  if (status === google.maps.DirectionsStatus.OK) {
-    directionsDisplay.setDirections(response);
-  } else {
-    window.alert('Directions request failed due to ' + status);
-  }
-});
-}*/
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay, start, end) {
 directionsService.route({
@@ -50,10 +58,7 @@ function getLocation(map){
 		navigator.geolocation.getCurrentPosition(function(position){
 			var location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 			map.setCenter(location);
-			new google.maps.Marker({
-	            position: location,
-	            map: map
-	    	}); 
+			createMarker(location, map, true); 
 		});
 	}
 }
@@ -70,6 +75,38 @@ function getRouteFromClick(map, marker){
 	}
 }
 
+function clearMarkers() {
+    setMapOnAll(null);
+}
+
+function setMapOnAll(map) {
+	for (var i = 0; i < markers.length; i++) {
+		markers[i].setMap(map);
+	}
+}
+
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
+
+function sortByResource(name, map){
+	deleteMarkers();
+	var resultArray = [];
+	var test1 = ['San Diego, CA'];
+	var test2 = ['Los Angeles, CA'];
+	var test3 = ['Las Vegas, NV'];
+	var test4 = ['Sacramento, CA'];
+	switch(name){
+		case 'Food': resultArray = test1; console.log("In 1");break;
+		case 'Water': resultArray = test2; console.log("In 2");break;
+		case 'Transportation': resultArray = test3; console.log("In 3");break;
+		case 'Supplies': resultArray = test4; console.log("In 4");break;
+	}
+
+	geoCodeMarkers(resultArray, map);
+}
+
 function openFindResources(){
 	openTab($("#find_resources"), $("#find_resources_btn"));
 	
@@ -79,28 +116,43 @@ function openFindResources(){
           center: {lat: 41.85, lng: -87.65},
           styles: [{"featureType":"administrative","stylers":[{"visibility":"off"}]},{"featureType":"poi","stylers":[{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"water","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","stylers":[{"visibility":"simplified"}]},{"featureType":"landscape","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"visibility":"off"}]},{"featureType":"road.local","stylers":[{"visibility":"on"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"water","stylers":[{"color":"#84afa3"},{"lightness":52}]},{"stylers":[{"saturation":-17},{"gamma":0.36}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]}]
         });
-
-        var userLocation = getLocation(map);
-
-        var addresses = ['2182 Avenida De La Playa La Jolla, CA 92037', '2236 Avenida De La Playa La Jolla, CA 92037', '4646 Convoy St San Diego, CA 92111'];
-        for(var x = 0; x < addresses.length; x++) {
-	        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-	            var p = data.results[0].geometry.location
-	            var latlng = new google.maps.LatLng(p.lat, p.lng);
-		        var marker = new google.maps.Marker({
-	                position: latlng,
-	                map: map
-	            });
-	            google.maps.event.addListener(marker, 'click', function(){getRouteFromClick(map, marker);});
-	        });
-    	}
-	}, 200);
-        /*var onChangeHandler = function() {
-          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+        var legend = document.getElementById('legend');
+        var icons = {
+          food: {
+            name: 'Food',
+            icon: iconBase + 'parking_lot_maps.png'
+          },
+          water: {
+            name: 'Water',
+            icon: iconBase + 'library_maps.png'
+          },
+          transportation: {
+            name: 'Transportation',
+            icon: iconBase + 'info-i_maps.png'
+          },
+          supplies: {
+            name: 'Supplies',
+            icon: iconBase + 'info-i_maps.png'
+          }
         };
-        document.getElementById('start').addEventListener('change', onChangeHandler);
-        document.getElementById('end').addEventListener('change', onChangeHandler);
-	}, 500);*/
+		var legend = document.getElementById('legend');
+
+        for (var key in icons) {
+          var type = icons[key];
+          var name = type.name;
+          var icon = type.icon;
+          var div = document.createElement('div');
+          div.name = name;
+          div.addEventListener("click", function(){sortByResource(this.name, map);});
+          div.innerHTML = '<img src="' + icon + '"> ' + name;
+          legend.appendChild(div);
+        }
+        var userLocation = getLocation(map);
+        map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(legend);
+        var addresses = ['2182 Avenida De La Playa La Jolla, CA 92037', '2236 Avenida De La Playa La Jolla, CA 92037', '4646 Convoy St San Diego, CA 92111'];
+        geoCodeMarkers(addresses, map);
+	}, 200);
 }
 
 function openSeeAnalysis(){
