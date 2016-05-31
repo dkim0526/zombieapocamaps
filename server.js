2a4c2ac172081e1284bbf45045a57e58285360bb
Daniel Kim <bjkim0526@gmail.com>
2016-05-31 11:31:39 -0700

    try with https for getlocation

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.use(session_middleware);
 passport.use(new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
-    callbackURL: "http://zombieapocamaps.herokuapp.com/auth/facebook/callback",
+    callbackURL: "https://zombieapocamaps.herokuapp.com/auth/facebook/callback",
     profileFields: ['id', 'name','picture.type(large)', 'emails', 'displayName', 'about', 'gender'],
     auth_type: "requthenticate"
 
