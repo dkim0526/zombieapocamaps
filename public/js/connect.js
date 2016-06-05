c01eb1af36b9d6ffebb4f5d427b25278f26e6a49
Jordan Yoshihara <jayoshih@ucsd.edu>
2016-06-05 11:01:14 -0700

    survival guide fixed

--- public/js/connect.js
+++ public/js/connect.js
@@ -9,50 +9,47 @@ $(document).ready(function() {
 	$("#connect_resources_btn").click(openResources);
 	$("#connect_health_btn").click(openHealth);
 	$("#connect_building_btn").click(openBuilding);
+	$("#connect_alliance_btn").click(openAlliances);
 	$("#connect_other_btn").click(openOther);
 	openKilling();
 })
 
 function openKilling(){
-	openPanel($("#connect_killing_btn"), "#67BA75");
+	openPanel($("#connect_killing_btn"), "#E6B35A", "#B19148", "#FEF7DD");
 }
 function openResources(){
-	openPanel($("#connect_resources_btn"), "#8B816C");
+	openPanel($("#connect_resources_btn"), "#67BA75", "#648C67", "#DDE7C5");
 }
 function openHealth(){
-	openPanel($("#connect_health_btn"), "#D6A191");
+	openPanel($("#connect_health_btn"), "#D6A191", "#AD5152", "#FEE0D6");
 }
 function openBuilding(){
-	openPanel($("#connect_building_btn"), "#715485");
-	setTimeout(initMap, 200);
+	openPanel($("#connect_building_btn"), "#A985B3", "#623C6D", "#F6E4F1");
+}
+function openAlliances(){
+	openPanel($("#connect_alliance_btn"), "#6395EC", "#0E4D92", "#CCE2EC");
 }
 function openOther(){
-	openPanel($("#connect_other_btn"), "#C3CC79");
-	setTimeout(initMap, 200);
+	openPanel($("#connect_other_btn"), "#AD805F", "#7B4E2D", "#F5EBE1");
 }
-function openPanel(button, color){
+function openPanel(button, color, secondColor, thirdColor){
 	if(!button.hasClass("active-profile-btn")){
 		$("#profile_tabs button").removeClass("active-profile-btn");
 		$("#profile_tabs button").css("background-color", "darkgray");
 		button.addClass("active-profile-btn");
 		button.css("background-color", color);
-		//$("#connect_content").css("border-color", color);
-		$("#connect_content").removeClass();
-		$("#connect_content").addClass(button.attr("id").toString().replace("_btn", ""));
+		$("#connect_content").css("border", "10px solid " + color);
+		$("#question_textarea").css("border-bottom", "3px solid " + secondColor);
+		$(".btn-custom").css("border", "4px solid " + secondColor);
+		$(".btn-custom").css("color", secondColor);
+		$(".btn-custom:hover, .divider").css("background-color",secondColor);
+		$(".question").css("border", "2px solid " + color);
+		$("#question_section").css("background-color", thirdColor);
 	}
 }
 
 function getLocation(map){
-	if(navigator.geolocation){
-		navigator.geolocation.getCurrentPosition(function(position){
-			var location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-			map.setCenter(location);
-			new google.maps.Marker({
-	            position: location,
-	            map: map
-	    	});
-		});
-	}
+
 }
 
 function initMap() {
