79e33303581fb0a8f3dd14122b18e176aac6d933
Chau Lucky Nguyen <lkn007@ucsd.edu>
2016-06-06 22:14:28 -0700

    updated help page, fixed red problems, changed images to gif

--- public/js/connect.js
+++ public/js/connect.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$("#connect_killing_btn").click(openKilling);
 	$("#connect_resources_btn").click(openResources);
-	$("#connect_health_btn").click(openHealth);
+	$("#connect_clinic_btn").click(openHealth);
 	$("#connect_building_btn").click(openBuilding);
 	$("#connect_alliance_btn").click(openAlliances);
 	$("#connect_other_btn").click(openOther);
@@ -24,7 +24,7 @@ function openResources(){
 	openPanel($("#connect_resources_btn"), "#67BA75", "#648C67", "#DDE7C5");
 }
 function openHealth(){
-	openPanel($("#connect_health_btn"), "#D6A191", "#AD5152", "#FEE0D6");
+	openPanel($("#connect_clinic_btn"), "#D6A191", "#AD5152", "#FEE0D6");
 }
 function openBuilding(){
 	openPanel($("#connect_building_btn"), "#A985B3", "#623C6D", "#F6E4F1");
